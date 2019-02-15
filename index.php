8125b3f95a108185517a3f2375ee6841a6ae1d8e
Ogbonna <o.vitalis@genesystechhub.com>
2019-02-15 10:22:51 +0100

    database test

--- index.php
+++ index.php
@@ -13,8 +13,22 @@
     }
     else{
         echo "database connection was successful";
-    }
-
+        
+        //sql to create table
+        $sql = "CREATE TABLE MyGuests (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        email VARCHAR(50),
+        reg_date TIMESTAMP
+        )";
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "Table MyGuests created successfully";
+        } else {
+            echo "Error creating table: " . $conn->error;
+        }
+}
     //include_once("home.html");
 
 ?>
