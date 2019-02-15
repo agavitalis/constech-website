a59873aa030eab23b88893c040c873a3f51031ca
Ogbonna <o.vitalis@genesystechhub.com>
2019-02-15 10:24:24 +0100

    database test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     $database = "heroku_93a392319043be9";
    //mysql://b435fd5a3df40e:ff1a102b@us-cdbr-iron-east-03.cleardb.net/heroku_93a392319043be9?reconnect=true
     // Create connection
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername, $username, $password,$database);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
