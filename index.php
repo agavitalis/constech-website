4de3cf0bffb933aba83dfec7896c948ecf77802e
Ogbonna <o.vitalis@genesystechhub.com>
2019-02-15 10:16:52 +0100

    testing db in prodecural php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php include_once("home.html");?>
+<?php 
+
+    $servername = "us-cdbr-iron-east-03.cleardb.net";
+    $username = "b435fd5a3df40e";
+    $password = "ff1a102b";
+    $database = "heroku_93a392319043be9";
+   //mysql://b435fd5a3df40e:ff1a102b@us-cdbr-iron-east-03.cleardb.net/heroku_93a392319043be9?reconnect=true
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else{
+        echo "database connection was successful";
+    }
+
+    //include_once("home.html");
+
+?>
